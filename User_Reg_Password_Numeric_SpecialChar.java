fe9970996caefeec4f21ffc16124585274c97f67
kush16816 <55789306+kush16816@users.noreply.github.com>
2020-10-07 09:15:38 +0530

    User Registration with password cases
    MinimumLimit - Use case for minimum 8 character limit.
    UpperCase - Use case for at least one uppercase character.
    Numeric_SpecialChar - Use case for at least one numeric and one special characters.

---
+++ User_Reg_Password_Numeric_SpecialChar.java
@@ -0,0 +1,59 @@
+
+package com.userReg;
+
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class User_Reg_Password_Numeric_SpecialChar {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter Your First Name ");
+		final String firstName = sc.next();
+		
+		final String fName = "^[A-Z][A-Za-z]*";
+		Matcher m = Pattern.compile(fName).matcher(firstName);
+		
+		System.out.println("Your first name is in valid format - " + m.matches());
+		
+
+		System.out.println("Enter Your Last Name ");
+		final String lastName = sc.next();
+		
+		final String lName = "^[A-Z][A-Za-z]*";
+		m = Pattern.compile(lName).matcher(lastName);
+
+		System.out.println("Your last name is in valid format - " + m.matches());
+		
+		System.out.println("Enter Your EmailID ");
+		final String emailID = sc.next();
+		
+		final String emlID = "^[a-z0-9]+[a-z0-9.+-_]+@[a-z0-9]+.[a-z]{2,}(.[a-z]{2})?$";
+		m = Pattern.compile(emlID).matcher(emailID);
+		
+		System.out.println("Your emailID is in valid format - " + m.matches());
+		
+		System.out.println("Enter Your Mobile ");
+		final String mobileNo = sc.nextLine();
+
+		final String mobNo = "[1-9][0-9][\\s][1-9][0-9]{9}";
+		m = Pattern.compile(mobNo).matcher(mobileNo);
+
+		System.out.println("Your mobile number is in valid format - " + m.matches());
+
+		System.out.println("Enter Your Password ");
+		final String password = sc.next();
+
+		final String pwd = "^(?=.*[A-Z])(?=.*[@$!#*])(?=.*[0-9]).{8,}";
+		m = Pattern.compile(pwd).matcher(password);
+
+		System.out.println("Your password is in valid format - " + m.matches());
+
+	}
+
+}
